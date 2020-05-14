96a2073472b29ad88a0f417a8044423f0a744770
BalisticksMain <65352971+BalisticksMain@users.noreply.github.com>
2020-05-15 02:43:36 +1200

    Update rtcKernelBridge.h

--- kern/rtcKernelBridge.h
+++ kern/rtcKernelBridge.h
@@ -2,5 +2,3 @@ int kbriRtcSetCurrentTick(unsigned int timestamp1, unsigned int timestamp2);
 int kbriRtcSetCurrentNetworkTick(unsigned int timestamp1, unsigned int timestamp2);
 int kbriRtcSetCurrentSecureTick(unsigned int timestamp1, unsigned int timestamp2);
 int kbriSblPostSsMgrSetCpRtc(unsigned int timestamp);
-int kbriDumpNvsAct(unsigned int fileno);
-int kbriRestoreNvsAct(unsigned int fileno);
